414e20ffafd52a1118b61bfdc2d7b49bf15363b6
Kwan Yui Chiu <kwan.chiu@kcl.ac.uk>
2022-09-29 10:31:17 +0100

    append number_of_primes check in primes.py and add .idea directory to ,gitignore

--- primes.py
+++ primes.py
@@ -2,5 +2,20 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes < 1:
+        raise ValueError(f"Number_of_primes:{number_of_primes} is less than or equal 0")
     list = []
+    counter = 2
+    while len(list) < number_of_primes:
+        # check if a number is prime
+        isPrime = True
+        j = 0
+        while isPrime and j < len(list):
+            if counter % list[j] == 0:
+                isPrime = False
+            j += 1
+
+        if isPrime:
+            list.append(counter)
+        counter += 1
     return list
